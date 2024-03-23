ac29f789814da3cb23c7e7df49ce71d484893c7e
YousefEldiasty <yoouseff.mohamed@gmail.com>
2024-03-23 02:52:53 +0200

    Second Commit

--- src/test/java/javaScript/Scroll.java
+++ src/test/java/javaScript/Scroll.java
@@ -4,6 +4,7 @@ import base.BaseTests;
 import org.testng.annotations.Test;
 
 public class Scroll extends BaseTests {
+    //Git Test
     @Test
     public void testScrollPage() throws InterruptedException {
         var largeAndDeepDom = homePage.clickLargeAndDeepDom();
